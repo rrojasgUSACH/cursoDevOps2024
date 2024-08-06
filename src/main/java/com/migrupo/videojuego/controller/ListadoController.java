d379dec00ab9c9fa3acf306fd4726bb63eb74f6d
Jazmín <j.gormaz@duocuc.cl>
2024-08-06 19:47:44 -0400

    Se modifica system out

--- src/main/java/com/migrupo/videojuego/controller/ListadoController.java
+++ src/main/java/com/migrupo/videojuego/controller/ListadoController.java
@@ -23,7 +23,7 @@ public class ListadoController {
     public String listarVideojuegos(Model model){
         List<Videojuego> destacados = videojuegoService.buscarDestacados();
         model.addAttribute("videojuegos", destacados);
-        System.out.println("destacados " + destacados);
+        System.out.println("videojuegos destacados " + destacados);
         return "listado";
     }
 }
