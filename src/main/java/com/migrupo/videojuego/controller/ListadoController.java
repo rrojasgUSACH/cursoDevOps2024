56105be97b3db966579893231aca481cde9890fa
Jazmín <j.gormaz@duocuc.cl>
2024-08-08 21:06:59 -0400

    Borrando comentario

--- src/main/java/com/migrupo/videojuego/controller/ListadoController.java
+++ src/main/java/com/migrupo/videojuego/controller/ListadoController.java
@@ -24,8 +24,7 @@ public class ListadoController {
         List<Videojuego> destacados = videojuegoService.buscarDestacados();
         model.addAttribute("videojuegos", destacados);
         System.out.println("videojuegos destacados " + destacados);
-        //for(int i=10; i < 10; i++)
-            System.out.println("saliendo del método ");
+        System.out.println("saliendo del método ");
         return "listado";
     }
 }
